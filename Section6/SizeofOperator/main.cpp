83e6591c67c9f1f71afb5f9a30bca0825f2fdd6f
Alfonso <alfonso.garcia2324@gmail.com>
2020-02-01 09:33:48 -0800

    added more to sizeof exercise

--- Section6/SizeofOperator/main.cpp
+++ Section6/SizeofOperator/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     
     cout << "sizeof information" << endl;
-    cout << "=============================" << endl;
+    cout << "========================================" << endl;
     
     cout << "char: " << sizeof(char) << " bytes." << endl;
     cout << "int: " << sizeof(int) << " bytes." << endl;
@@ -19,5 +19,15 @@ int main() {
     
     cout << "float: " << sizeof(float) << " bytes." << endl;
     cout << "double: " << sizeof(double) << " bytes." << endl;
-    cout << "long double: " << sizeof(long double) << " bytes." << endl;
+    cout << "long double: " << sizeof(long double) << " bytes." << endl;\
+    
+    // values defined in <climits>
+    cout << "========================================" << endl;
+    
+    cout << "Minimum values:" << endl;
+    cout << "char: " << CHAR_MIN << endl;
+    cout << "int: " << INT_MIN << endl;
+    cout << "short: " << SHRT_MIN << endl;
+    cout << "long: " << LONG_MIN << endl;
+    cout << "long long: " << LLONG_MIN << endl;
 }
