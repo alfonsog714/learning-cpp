23e1af0939826bbb51022182504985d3bf3f5906
Alfonso <alfonso.garcia2324@gmail.com>
2020-02-03 10:30:32 -0800

    still working on constants

--- Section6/Constants/main.cpp
+++ Section6/Constants/main.cpp
@@ -35,13 +35,19 @@ using namespace std;
 int main() {
     cout << "Hello, welcome to Frank's Carpet Cleaning Service." << endl;
     cout << "==================================================" << endl;
-    cout << "How many rooms would you like cleaned?";
+    cout << "How many rooms would you like cleaned? ";
     
     int number_of_rooms{0};
     cin >> number_of_rooms;
     
     cout << "\nEstimate for carpet cleaning service" << endl;
     cout << "Number of rooms: " << number_of_rooms << endl;
+    cout << "Price per room: $" << 30 << endl;
+    cout << "Cost: $" << 30 * number_of_rooms << endl;
+    cout << "Tax: $" << 30 * number_of_rooms * 0.06 << endl;
+    cout << "==================================================" << endl;
+    cout << "Total estimate is $" << (30 * number_of_rooms) + (30 * number_of_rooms * 0.06) << endl;
+    cout << "This estimate is valid for " << 30 << " days." << endl;
     
     cout << endl;
     return 0;
